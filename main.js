2767059ee4615da7956317d68d44011329a0fe0d
シューリ ピーター <shuripita@schurymacbook2.wnsc.kek.jp>
2026-06-19 20:49:34 +0900

    Fixed Windows window size

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ const path = require('node:path')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: process.platform === 'win32' ? 760 : 735,
-    height: process.platform === 'win32' ? 760 : 730,
+    width: process.platform === 'win32' ? 750 : 735,
+    height: process.platform === 'win32' ? 745 : 730,
     useContentSize: true,
     resizable: false,
     fullscreenable: false,
